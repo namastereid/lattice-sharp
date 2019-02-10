f932bc17dd3dca6d63d10230acb3e05e851ee721
Namaste Reid <namaste.reid@gmail.com>
2019-02-10 06:47:38 -0700

    I can launch a window, yay!

---
+++ lattice-sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lattice_sharp
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using(var game = new Lattice())
+                game.Run();
+        }
+    }
+}
