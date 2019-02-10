c837e81b86421e440f572e9181b5463bcbb2c238
Namaste Reid <namaste.reid@gmail.com>
2019-02-10 07:38:13 -0700

    implement alt+enter to toggle fullscreen

--- lattice-sharp/Lattice.cs
+++ lattice-sharp/Lattice.cs
@@ -1,52 +1,98 @@
+using System;
+using System.Linq;
+using SharpDX.WIC;
+
 namespace lattice_sharp
 {
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
-//    using Microsoft.Xna.Framework.Storage;
     using Microsoft.Xna.Framework.Input;
 
     public class Lattice : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _texture;
+        private bool _toggleFullscreen;
 
         public Lattice()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.HardwareModeSwitch = false;
+            Window.AllowUserResizing = true;
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _texture = CreateTexture(_graphics.GraphicsDevice, 32, 32, pixel => Color.Black);
 
             // TODO: use this.Content to load your game content here
         }
-        
+
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            var keyState = Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                keyState.IsKeyDown(Keys.Escape))
                 Exit();
- 
+
+            if ((keyState.IsKeyDown(Keys.LeftAlt) || keyState.IsKeyDown(Keys.RightAlt))
+                && keyState.IsKeyDown(Keys.Enter))
+            {
+                _toggleFullscreen = true;
+            }
+
+            if (_toggleFullscreen &&
+                (keyState.IsKeyUp(Keys.LeftAlt) &&
+                 keyState.IsKeyUp(Keys.RightAlt)
+                 || keyState.IsKeyUp(Keys.Enter)))
+            {
+                _graphics.ToggleFullScreen();
+                _toggleFullscreen = false;
+            }
+
             // TODO: Add your update logic here
- 
+
             base.Update(gameTime);
         }
-        
+
         protected override void Draw(GameTime gameTime)
         {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
- 
+            _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture, Vector2.Zero, Color.Blue);
+            _spriteBatch.End();
+
             // TODO: Add your drawing code here
- 
+
             base.Draw(gameTime);
+        }
+
+        private static Texture2D CreateTexture(GraphicsDevice device, int width, int height, Func<int, Color> paint)
+        {
+            //initialize a texture
+            var texture = new Texture2D(device, width, height);
+
+            //the array holds the color for each pixel in the texture
+            var data = new Color[width * height];
+            for (var pixel = 0; pixel < data.Length; pixel++)
+            {
+                //the function applies the color according to the specified pixel
+                data[pixel] = paint(pixel);
+            }
+
+            //set the color
+            texture.SetData(data);
+
+            return texture;
         }
     }
 }
