f0cbf8ae15045089799d24352b20d75b9af81239
Namaste Reid <namaste.reid@gmail.com>
2019-02-10 20:16:00 -0700

    fixed 4:3 aspect ratio, render square grid

--- lattice-sharp/Lattice.cs
+++ lattice-sharp/Lattice.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 
 namespace lattice_sharp
 {
@@ -16,17 +15,20 @@ namespace lattice_sharp
 
         public Lattice()
         {
-            _graphics = new GraphicsDeviceManager(this) {HardwareModeSwitch = false};
-            Window.AllowUserResizing = true;
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                HardwareModeSwitch = false,
+                PreferredBackBufferWidth = 1920,
+                PreferredBackBufferHeight = 1080
+            };
+            
             Content.RootDirectory = "Content";
         }
 
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            
-            _texture = CreateTexture(_graphics.GraphicsDevice, 30, 30);
+            _texture = CreateTexture(_graphics.GraphicsDevice, 40, 30);
         }
 
         protected override void Update(GameTime gameTime)
@@ -58,7 +60,9 @@ namespace lattice_sharp
         {
             _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
-            _spriteBatch.Draw(_texture, new Rectangle(0, 0, _graphics.GraphicsDevice.Viewport.Width, _graphics.GraphicsDevice.Viewport.Height), Color.White);
+            _spriteBatch.Draw(_texture,
+                new Rectangle(0, 0, _graphics.GraphicsDevice.Viewport.Width, _graphics.GraphicsDevice.Viewport.Height),
+                Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -66,26 +70,24 @@ namespace lattice_sharp
 
         private static Texture2D CreateTexture(GraphicsDevice device, int width, int height)
         {
-            //initialize a texture
             var texture = new Texture2D(device, width, height);
 
-            //the array holds the color for each pixel in the texture
             var data = new Color[width * height];
             var black = true;
             var row = 0;
             for (var pixel = 0; pixel < data.Length; pixel++)
             {
-                var factor = pixel / height;
+                var factor = pixel / width;
                 if (factor > row) // new row
                 {
                     row = factor;
                     black = factor % 2 == 0;
                 }
+
                 data[pixel] = black ? Color.Black : Color.White;
                 black = !black;
             }
 
-            //set the color
             texture.SetData(data);
 
             return texture;
